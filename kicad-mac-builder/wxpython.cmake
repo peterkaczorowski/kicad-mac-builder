29bc83e048d4c2c4f17bb193ae17cafcbeb309b9
Adam Wolf <adamwolf@feelslikeburning.com>
2019-12-24 21:28:05 -0600

    Add --enable-richtext to wxwidgets configuration.

--- kicad-mac-builder/wxpython.cmake
+++ kicad-mac-builder/wxpython.cmake
@@ -20,6 +20,7 @@ ExternalProject_Add(
                             --enable-aui
                             --enable-html
                             --enable-stl
+			    --enable-richtext
                             --disable-mediactrl
                             --with-libjpeg=builtin
                             --with-libpng=builtin
