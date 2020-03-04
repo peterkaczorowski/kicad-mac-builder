ec717ea24cfe5ab38e133b66a047815a49673251
Adam Wolf <adamwolf@feelslikeburning.com>
2020-03-04 13:32:06 -0600

    Stop version pinning glm. Cleanup.

--- ci/src/bootstrap.sh
+++ ci/src/bootstrap.sh
@@ -14,11 +14,5 @@ done
 
 PATH=$PATH:/usr/local/bin
 export HOMEBREW_NO_ANALYTICS=1
-#echo "Installing cmake 3.6.2 for testing"
-#brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/eaf0d6c78c8d49ffeaa158eb307101f034944795/Formula/cmake.rb
-#cmake --version
-# version pinning glm
-echo "Installing glm 0.9.9.2"
-brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/9ce61eaa2776d0ab011e0559a86afff588f6eccb/Formula/glm.rb
 echo "Installing some dependencies"
-brew install glew cairo doxygen gettext wget bison libtool autoconf automake cmake swig opencascade boost
+brew install glew cairo doxygen gettext wget bison libtool autoconf automake cmake swig opencascade boost glm
