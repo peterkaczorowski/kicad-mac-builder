bc21110733969b246e23196a032b2416808739a6
Nick Østergaard <oe.nick@gmail.com>
2020-05-04 22:12:43 +0200

    Add a 30 minute timeout for installing openssl

--- ci/src/bootstrap.sh
+++ ci/src/bootstrap.sh
@@ -2,6 +2,9 @@
 set -x
 set -e
 
+# Easy hack to get a timeout command
+function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+
 for _ in 1 2 3; do
   if ! command -v brew >/dev/null; then
     echo "Installing Homebrew ..."
@@ -15,7 +18,7 @@ done
 PATH=$PATH:/usr/local/bin
 export HOMEBREW_NO_ANALYTICS=1
 echo "Installing openssl to debug it, it often fails on make test..."
-brew install --verbose openssl
+timeout 1800 brew install --verbose openssl
 echo "Installing some dependencies"
 brew install glew cairo doxygen gettext wget bison libtool autoconf automake cmake swig boost glm
 brew install -f /vagrant/external/oce*tar.gz
