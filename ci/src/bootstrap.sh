4272d86c55c3266959bd893b63e2e872979fe54e
Adam Wolf <adamwolf@feelslikeburning.com>
2020-06-11 23:09:15 -0500

    Remove openssl special handling after VMs updated, and update OpenSSH.

--- ci/src/bootstrap.sh
+++ ci/src/bootstrap.sh
@@ -17,8 +17,8 @@ done
 
 PATH=$PATH:/usr/local/bin
 export HOMEBREW_NO_ANALYTICS=1
-echo "Installing openssl to debug it, it often fails on make test..."
-timeout 1800 brew install --verbose openssl
+echo "Updating SSH"
+brew install openssh
 echo "Installing some dependencies"
-brew install glew cairo doxygen gettext wget bison libtool autoconf automake cmake swig boost glm
+brew install glew cairo doxygen gettext wget bison libtool autoconf automake cmake swig boost glm openssl
 brew install -f /vagrant/external/oce*tar.gz
