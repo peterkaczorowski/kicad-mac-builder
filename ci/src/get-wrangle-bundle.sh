7ebf5b611fc2cce2fb9ccf45b2a490dbff183dab
Adam Wolf <adamwolf@feelslikeburning.com>
2022-07-01 15:50:10 -0500

    Change an errant python to python3
    (cherry picked from commit a31a28b53210ad5ce6ef74b4650ad00edb9ad5b5)

--- ci/src/get-wrangle-bundle.sh
+++ ci/src/get-wrangle-bundle.sh
@@ -9,5 +9,5 @@ python3 -m venv virtualenv
 echo "Entering it..."
 source virtualenv/bin/activate
 echo "Installing dyldstyle..."
-python -m pip install git+https://gitlab.com/adamwwolf/dyldstyle.git
+python3 -m pip install git+https://gitlab.com/adamwwolf/dyldstyle.git
 echo "dyldstyle has been installed.  wrangle-bundle should be available at $(pwd)/virtualenv/bin/wrangle-bundle."
