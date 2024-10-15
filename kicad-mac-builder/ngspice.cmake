55c98079ed69e869296e82cde7617c9f17ee8afa
Jon Evans <jon@craftyjon.com>
2024-10-14 20:05:52 -0400

    Bump ngspice; build without OpenMP
    See https://gitlab.com/kicad/code/kicad/-/issues/18866

--- kicad-mac-builder/ngspice.cmake
+++ kicad-mac-builder/ngspice.cmake
@@ -4,14 +4,13 @@ ExternalProject_Add(
     ngspice
     PREFIX  ngspice
     GIT_REPOSITORY git://git.code.sf.net/p/ngspice/ngspice
-    GIT_TAG ngspice-42
+    GIT_TAG ngspice-43
     UPDATE_COMMAND      ""
     PATCH_COMMAND       ""
     CONFIGURE_COMMAND ./autogen.sh
     COMMAND ./configure --prefix=${ngspice_INSTALL_DIR} --with-ngshared
-        --enable-xspice --enable-cider --disable-debug
-        LDFLAGS=-L/usr/local/opt/bison/lib:-L/opt/homebrew/opt/libomp/lib
-        CPPFLAGS=-I/opt/homebrew/opt/libomp/include
+        --enable-xspice --enable-cider --disable-debug --disable-openmp
+        LDFLAGS=-L/usr/local/opt/bison/lib
     BUILD_COMMAND  ${BIN_DIR}/run-with-path.sh ${CMAKE_MAKE_PROGRAM} /usr/local/opt/bison/bin:$ENV{PATH}
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND make install
