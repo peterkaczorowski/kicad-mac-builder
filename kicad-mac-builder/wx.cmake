2e1f6d0509f4e4f111b5288f7617bdf1b7e548db
Peter D. Kaczorowski <piotr.kaczorowski@gmail.com>
2025-01-21 20:39:29 +0100

    new wxWidget library with libpng 1.6.45 + APNG patch

--- kicad-mac-builder/wx.cmake
+++ kicad-mac-builder/wx.cmake
@@ -12,8 +12,8 @@ endif()
 ExternalProject_Add(
     wxwidgets
     PREFIX  wxwidgets
-    GIT_REPOSITORY https://gitlab.com/kicad/code/wxWidgets.git
-    GIT_TAG kicad/macos-wx-3.2
+    GIT_REPOSITORY https://github.com/peterkaczorowski/wxWidgets_kicad_macos-wx-3.2
+    GIT_TAG kicad/macos-wx-3.2-apng
     CONFIGURE_COMMAND   CPPFLAGS=-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=1 MAC_OS_X_VERSION_MIN_REQUIRED=${MACOS_MIN_VERSION} CC=clang CXX=clang++ ./configure
                         --prefix=${wxwidgets_INSTALL_DIR}
                         --with-macosx-version-min=${MACOS_MIN_VERSION}
